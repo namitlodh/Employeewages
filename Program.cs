9ee543c7c537b690c1f920b4a3599185905a1a4c
unknown <namitlodh>
2024-01-19 15:06:16 +0530

    Use Case 1: Absent or Present

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, 2);
+            if (a == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            Console.ReadLine();
+        }
+    }
+}
