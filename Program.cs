214f684b23e35d1d8d671fdaf2a6467cb0344efd
unknown <namitlodh>
2024-01-19 15:12:25 +0530

    Use Case 3: Part Time wage

--- Program.cs
+++ Program.cs
@@ -10,11 +10,30 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter wage per hour : ");
-            String wageperhour = Console.ReadLine();
-            Console.WriteLine("Enter total hours worked : ");
-            String hours = Console.ReadLine();
-            Console.WriteLine(Convert.ToInt32(wageperhour) * Convert.ToInt32(hours));
+            Random random = new Random();
+            int isPresent = random.Next(0, 3);
+            if (isPresent == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else if (isPresent == 1)
+            {
+                Console.WriteLine("Employee is working Full Time");
+                Console.WriteLine("Enter employee wage: ");
+                string wage = Console.ReadLine();
+                Console.WriteLine("Enter total hours");
+                string hours = Console.ReadLine();
+                Console.WriteLine(Convert.ToInt32(wage) * Convert.ToInt32(hours));
+            }
+            else
+            {
+                Console.WriteLine("Employee is working Part time");
+                Console.WriteLine("Enter employee wage: ");
+                string wageparttime = Console.ReadLine();
+                Console.WriteLine("Enter total hours");
+                string hoursparttime = Console.ReadLine();
+                Console.WriteLine(Convert.ToInt32(wageparttime) * Convert.ToInt32(hoursparttime));
+            }
             Console.ReadLine();
         }
     }
